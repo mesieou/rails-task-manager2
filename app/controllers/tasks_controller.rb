b67fd8cea895192b6ac407f89cc5d9e6b2ebd475
juan bernal <mesieou@gmail.com>
2024-01-17 08:36:38 +1100

    added all the crud actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,41 @@
 class TasksController < ApplicationController
+  before_action
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
