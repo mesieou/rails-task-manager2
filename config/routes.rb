beb5ac947732da4125b9908a73a51d0360e9a6b7
juan bernal <mesieou@gmail.com>
2024-01-17 11:12:43 +1100

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
-  get '/tasks/:id/edit', to: 'tasks#edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
   patch '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy'
 end
